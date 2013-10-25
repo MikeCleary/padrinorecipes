5a1a0fdcb07f5fdc82cd861b4462701d0fe59f2d
Michael <Cleary481@gmail.com>
2013-10-25 14:39:45 +0100

    controllers generated

---
+++ app/controllers/recipes.rb
@@ -0,0 +1,23 @@
+PadrinoRecipes::App.controllers :recipes do
+  
+  get :index do
+
+  end
+
+  get :new do
+
+  end
+
+  post :create do
+
+  end
+
+  put :update do
+
+  end
+
+  delete :destroy do
+
+  end
+
+end
